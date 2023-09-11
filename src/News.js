bff5d9a24de75c5fb8e88e36efda70210ce9c14e
Saikat Samanta <95569573+psykat1116@users.noreply.github.com>
2023-09-11 16:59:34 +0530

    update

--- src/News.js
+++ src/News.js
@@ -20,84 +20,30 @@ const News=(props)=>{
     //eslint-disable-next-line
   }, [])
   
-  // async componentDidMount() {
-  //   let url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.catagory}&apiKey=ea6c135a167440daace246c3c7fcedca&page=1&pageSize=${props.pageSize}`
-  //   this.setState({loading:true})
-  //   let data = await fetch(url);
-  //   let parseData = await data.json()
-  //   this.setState({articles: parseData.articles,totalArticles: parseData.totalResults,loading: false})
-  //   this.updateNews();
-  // }
 
   const updateNews=async()=>{
     props.setProgress(0);
     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.catagory}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
-    // this.setState({ loading: true });
     setLoading(true)
     let data = await fetch(url);
     let parseData = await data.json();
     setArticle(parseData.articles);
     setLoading(false)
     settotalArticles(parseData.totalArticles)
-    // this.setState({ articles: parseData.articles });
-    // this.setState({
-    //   loading: false,
-    //   totalArticles: parseData.totalResults,
-    // });
     props.setProgress(100)
   }
 
-  // const handlePrevious = async () => {
-  //   // let url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.catagory}&apiKey=ea6c135a167440daace246c3c7fcedca&page=${this.state.page-1}&pageSize=${props.pageSize}`
-  //   // this.setState({loading:true})
-  //   // let data = await fetch(url);
-  //   // let parseData = await data.json()
-  //   // this.setState({articles: parseData.articles})
-  //   // this.setState({
-  //   //   page: this.state.page-1,
-  //   //   loading: false
-  //   // })
-  //   // this.setState({ page: this.state.page - 1 });
-  //   setPage(page-1)
-  //   updateNews();
-  // };
-
-  // const handleNext = async () => {
-  //   // if(this.state.page+1 <= Math.ceil(this.state.totalArticles/props.pageSize))
-  //   // {
-  //   //   let url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.catagory}&apiKey=ea6c135a167440daace246c3c7fcedca&page=${this.state.page+1}&pageSize=${props.pageSize}`
-  //   //   this.setState({loading:true})
-  //   //   let data = await fetch(url);
-  //   //   let parseData = await data.json()
-  //   //   this.setState({articles: parseData.articles})
-  //   //   this.setState({
-  //   //     page: this.state.page+1,
-  //   //     loading: false
-  //   //   })
-  //   // }
-  //   // this.setState({ page: this.state.page + 1 });
-  //   setPage(page+1)
-  //   updateNews();
-  // };
-
   const fetchMoreData = async () =>{
-    // this.setState({page: this.state.page + 1});
     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.catagory}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
     setPage(page+1);
-    // this.setState({ loading: true });
     setLoading(true);
     let data = await fetch(url);
     props.setProgress(30);
     let parseData = await data.json();
     props.setProgress(60);
-    // this.setState({ articles: this.state.articles.concat(parseData.articles)});
     setArticle(article.concat(parseData.articles))
     setLoading(false)
     settotalArticles(parseData.totalArticles)
-    // this.setState({
-    //   loading: false,
-    //   totalArticles: parseData.totalResults,
-    // });
     props.setProgress(100);
   }
 
@@ -108,9 +54,9 @@ const News=(props)=>{
         </h1>
         {/* {this.state.loading && <Spinner />} */}
         <InfiniteScroll
-          dataLength={article.length}
+          dataLength={article && article.length}
           next={fetchMoreData}
-          hasMore={article.length !== totalArticles}
+          hasMore={article && article.length !== totalArticles}
           loader={<Spinner/>}
         >
         <div className="container">
@@ -132,28 +78,6 @@ const News=(props)=>{
           </div>
           </div>
         </InfiniteScroll>
-        {/* <div className="container d-flex justify-content-between my-5">
-          <button
-            type="button"
-            disabled={this.state.page <= 1}
-            className="btn btn-dark"
-            onClick={this.handlePrevious}
-          >
-            {" "}
-            &larr; Previous
-          </button>
-          <button
-            type="button"
-            disabled={
-              this.state.page + 1 >
-              Math.ceil(this.state.totalResults / props.pageSize)
-            }
-            className="btn btn-dark"
-            onClick={this.handleNext}
-          >
-            Next &rarr;
-          </button>
-        </div> */}
       </>
     );
   }
@@ -163,11 +87,5 @@ News.defualtProps = {
   pageSize: 8,
   catagory: "general",
 };
-
-// News.PropTypes = {
-//   country: PropTypes.string,
-//   pageSize: PropTypes.number,
-//   category: PropTypes.string,
-// };
 
 export default News;
